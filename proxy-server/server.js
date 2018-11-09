645bf5191e0fc56dcd6f0e36254ef1c53d18b90a
Caitlin Detro <caitlindetro@gmail.com>
2018-11-09 00:18:26 -0600

    Updated proxy server

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -13,12 +13,10 @@ app.get('/:houseid', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../public/index.html`))
 });
 
-app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
-});
-
-
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../public/index.html`))
+});
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
 });
