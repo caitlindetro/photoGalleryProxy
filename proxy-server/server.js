44c9f47111b618c1f45abb53ef5f0ac64d2f91e6
Caitlin Detro <caitlindetro@gmail.com>
2018-11-08 21:04:24 -0600

    Console logging server

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -6,7 +6,8 @@ const app = express();
 const port = 8081;
 
 app.use(cors);
-app.use(express.static(path.join(__dirname, 'public')));
+console.log('****:', __dirname);
+app.use(express.static(path.join(`${__dirname}/public`)));
 
 app.get('/:listingId', (req, res) => {
   res.sendFile(path.join(`${__dirname}/public/index.html`));
